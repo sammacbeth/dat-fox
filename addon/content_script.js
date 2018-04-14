30da7d45951e44c376dc91cd9024dd18535c4791
Sam Macbeth <sam@macbeth.cc>
2018-04-14 18:58:16 +0200

    dat:// scripts which fail to load require new script tags.

--- addon/content_script.js
+++ addon/content_script.js
@@ -20,16 +20,21 @@ exportFunction(postMessage, window, { defineAs: '_datfoxPostMessage' });
 exportFunction(addListener, window, { defineAs: '_datfoxAddListener' });
 
 // inject datArchive script into the page
-const scriptTag = document.createElement('script')
+const scriptTag = document.createElement('script');
 scriptTag.src = browser.extension.getURL('dat-archive.js');
 const target = document.head || document.documentElement;
-target.appendChild(scriptTag)
+target.appendChild(scriptTag);
 scriptTag.parentNode.removeChild(scriptTag);
 
 // rewrite dat urls in the document to http
 [...document.scripts]
 .filter(e => e.src.startsWith('dat://'))
-.forEach(e => e.src = e.src.replace('dat://', 'http://'));
+.forEach(e => {
+    // write a new script element with a http url
+    const scriptTag = document.createElement('script')
+    scriptTag.src = e.src.replace('dat://', 'http://');
+    e.parentNode.appendChild(scriptTag);
+});
 [...document.images]
 .filter(e => e.src.startsWith('dat://'))
 .forEach(e => e.src = e.src.replace('dat://', 'http://'));
