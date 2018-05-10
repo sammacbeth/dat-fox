2ab1ddb5a69c42a19e5d5d7670b10f24ed8d8a32
Sam Macbeth <sammacbeth@gmail.com>
2018-05-10 18:11:56 +0200

    Add a view local files button for owned dats.

--- popup/popup.js
+++ popup/popup.js
@@ -15,10 +15,34 @@ getTab().then(async (tab) => {
     document.getElementById('title').innerText = title || '';
     document.getElementById('description').innerText = description || '';
     document.getElementById('peers').innerText = `Peers: ${peers}`;
-    document.getElementById('fork-button').onclick = async () => {
-        const forkedArchive = await DatArchive.fork(archive.url, {});
-        browser.tabs.update(tab.id, {
-            url: forkedArchive.url,
-        });
+    // action buttons
+    const actionsElem = document.getElementById('actions');
+    if (isOwner) {
+        // View local files button - opens the file url for the dat folder on the local system
+        const library = await bridge.postMessage({ action: 'listLibrary' });
+        const [,, datAddr] = archive.url.split('/');
+        const libraryEntry = library.find(({ url }) => url === `dat://${datAddr}`);
+        if (libraryEntry) {
+            const filesButton = document.createElement('button');
+            filesButton.setAttribute('class', 'btn');
+            filesButton.innerText = 'View local files';
+            filesButton.onclick = () => {
+                browser.tabs.create({
+                    url: `${libraryEntry.dir}`,
+                });
+            };
+            actionsElem.appendChild(filesButton);
+        }
+    } else {
+        const forkButton = document.createElement('button');
+        forkButton.setAttribute('class', 'btn');
+        forkButton.innerText = 'Fork Site';
+        forkButton.onclick = async () => {
+            const forkedArchive = await DatArchive.fork(archive.url, {});
+            browser.tabs.update(tab.id, {
+                url: forkedArchive.url,
+            });
+        };
+        actionsElem.appendChild(forkButton);
     }
 });
