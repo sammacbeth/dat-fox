175bebd0d45d2001693a4af10258364b923d79a6
Sam Macbeth <sammacbeth@gmail.com>
2018-07-18 10:33:15 +0200

    Remove content-sniffing: it's now working in libdweb.

--- background/protocol.js
+++ background/protocol.js
@@ -1,34 +1,31 @@
 /**
  * Sets up redirects for dat:// urls to be proxied.
  */
-import mime from 'mime';
 import { addDatSite, datSites } from './sites';
 import { showDatSecureIcon } from './page-action';
 
 const datUrlMatcher = /^[0-9a-f]{64}(\+[0-9]+)?$/;
 
 function init() {
-    browser.protocol.registerProtocol('dat', async (request) => {
+    browser.protocol.registerProtocol('dat', (request) => {
         const url = request.url.replace('dat://', 'http://');
-        const path = url.split('?')[0];
-        let contentType = mime.getType(path);
-        if (path.endsWith('/')) {
-            contentType = 'text/html';
-        }
-        console.log('xxx', url, contentType);
         return {
-            contentType,
-            content: (async function*() {
-                const contentBlob = await (await fetch(url)).blob();
-                const buffer = await new Promise((resolve) => {
-                    const reader = new FileReader();
-                    reader.onload = () => {
-                        resolve(reader.result);
+            content: {
+                async next() {
+                    const contentBlob = await (await fetch(url)).blob();
+                    const buffer = await new Promise((resolve) => {
+                        const reader = new FileReader();
+                        reader.onload = () => {
+                            resolve(reader.result);
+                        }
+                        reader.readAsArrayBuffer(contentBlob);
+                    });
+                    return {
+                        done: true,
+                        value: buffer,
                     }
-                    reader.readAsArrayBuffer(contentBlob);
-                });
-                yield buffer;
-            })()
+                }
+            }
         };
     });
 
