e460dd3b8f3556b74f3b797a461bde48469345c3
Sam Macbeth <sam@cliqz.com>
2019-12-27 21:55:18 +0100

    Switch to new proxy.onRequest API

--- background/proxy.js
+++ background/proxy.js
@@ -1,18 +1,36 @@
 const pacFile = 'pac.js';
 const pacFileUrl = browser.extension.getURL(pacFile);
+const proxyOnRequestAvailable = !!browser.proxy.onRequest;
+const datSites = new Set();
+const datUrlMatcher = /^[0-9a-f]{64}$/;
+const proxyInfo = {
+    type: 'http',
+    host: 'localhost',
+    port: 3000,
+};
 
 // in order to create a valid origin for dat sites we need to 'invent'
 // hostnames for them. We can do this via a pac file which proxies requests
 // for domains we want to serve over dat to the dat-gateway. This hack allows
 // us to create hostnames on the fly.
-export let proxyReady = browser.proxy.register(pacFile);
+export let proxyReady = proxyOnRequestAvailable ? Promise.resolve() : browser.proxy.register(pacFile);
 
-// log messages from the pac file
-browser.runtime.onMessage.addListener((message, sender) => {
-    if (sender.url === pacFileUrl) {
-        console.log('[PAC]', message);
-    }
-});
+if (proxyOnRequestAvailable) {
+    browser.proxy.onRequest.addListener((details) => {
+        const { host } = new URL(details.url);
+        if (datSites.has(host) || datUrlMatcher.test(host)) {
+            console.log(`loading url over dat: ${details.url} via ${proxyInfo.type}://${proxyInfo.host}:${proxyInfo.port}`);
+            return proxyInfo;
+        }
+    }, { 'urls': ['http://*/*']} );
+} else {
+    // log messages from the pac file
+    browser.runtime.onMessage.addListener((message, sender) => {
+        if (sender.url === pacFileUrl) {
+            console.log('[PAC]', message);
+        }
+    });
+}
 
 export function setGatewayAddress(address) {
     const addressRegex = /(https?):\/\/([^/?:]+)(:[0-9]+)?/;
@@ -32,5 +50,17 @@ export function setGatewayAddress(address) {
 }
 
 export function sendMessageToPAC(message) {
-    browser.runtime.sendMessage(message, { toProxyScript: true });
+    if (proxyOnRequestAvailable) {
+        if (message.action === 'add') {
+            datSites.add(message.host);
+        } else if (message.action === 'remove') {
+            datSites.delete(message.host);
+        } else if (message.action === 'setGateway') {
+            proxyInfo.type = message.type;
+            proxyInfo.host = message.host;
+            proxyInfo.port = message.port;
+        }
+    } else {
+        browser.runtime.sendMessage(message, { toProxyScript: true });
+    }
 }
